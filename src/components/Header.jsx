afe56f34a97a021daf1bc0d08097c9214db41453
Ihor Chekhunov <Ihor Chekhunov.scdesignk@gmail.com>
2021-05-25 22:15:23 +0300

    fixed all progect and change bd structure and fixed props dependencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-import logo from '../images/logo.svg';
+import logo from '../images/logo.png';
 
 function Navigation() {
   return (
--- src/state/state.js
+++ src/state/state.js
@@ -6,6 +6,41 @@ let state = {
       quiz_title: 'Сортування сміття',
       quiz_class: '1-4',
       color: '#86E3CE',
+      questions: [
+        {
+          id: 0,
+          id_quiz: '0',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+        {
+          id: 1,
+          id_quiz: '0',
+          img_quest: '../img/ques_img_1.png',
+          question_title: 'виробництві яко використовується переробленний пластик?',
+          answers: [{ answer: 'поліестер' }, { answer: 'бабовна' }, { answer: 'інакше' }],
+        },
+        {
+          id: 2,
+          id_quiz: '0',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+      ],
     },
     {
       id: 1,
@@ -13,6 +48,41 @@ let state = {
       quiz_title: 'Переробка сміття',
       quiz_class: '5-7',
       color: '#FFDD95',
+      questions: [
+        {
+          id: 0,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+        {
+          id: 1,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title: 'виробництві яко використовується переробленний пластик?',
+          answers: [{ answer: 'поліестер' }, { answer: 'бабовна' }, { answer: 'інакше' }],
+        },
+        {
+          id: 2,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+      ],
     },
     {
       id: 2,
@@ -20,7 +90,41 @@ let state = {
       quiz_title: 'Екологія світу',
       quiz_class: '8-10',
       color: '#D0E6A5',
-      questions: '',
+      questions: [
+        {
+          id: 0,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+        {
+          id: 1,
+          id_quiz: '0',
+          img_quest: '../img/ques_img_1.png',
+          question_title: 'виробництві яко використовується переробленний пластик?',
+          answers: [{ answer: 'поліестер' }, { answer: 'бабовна' }, { answer: 'інакше' }],
+        },
+        {
+          id: 2,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+      ],
     },
     {
       id: 3,
@@ -28,81 +132,41 @@ let state = {
       quiz_title: 'Вплив екології на людину.',
       quiz_class: '11-12',
       color: '#FC887B',
-      questions: '',
-    },
-  ],
-
-  questions: [
-    {
-      id: 0,
-      id_quiz: '0',
-      img_quest: '../img/ques_img_1.png',
-      question_title:
-        'Як називається тканина, у виробництві яко використовується переробленний пластик?',
-      answers: [
-        { answer: 'шовк' },
-        { answer: 'бабовна' },
-        { answer: 'поліестер' },
-        { answer: 'інакше' },
+      questions: [
+        {
+          id: 0,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
+        {
+          id: 1,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title: 'виробництві яко використовується переробленний пластик?',
+          answers: [{ answer: 'поліестер' }, { answer: 'бабовна' }, { answer: 'інакше' }],
+        },
+        {
+          id: 2,
+          id_quiz: '1',
+          img_quest: '../img/ques_img_1.png',
+          question_title:
+            'Як називається тканина, у виробництві яко використовується переробленний пластик?',
+          answers: [
+            { answer: 'шовк' },
+            { answer: 'бабовна' },
+            { answer: 'поліестер' },
+            { answer: 'інакше' },
+          ],
+        },
       ],
-    },
-    {
-      id: 1,
-      id_quiz: '0',
-      img_quest: '../img/ques_img_1.png',
-      question_title: 'виробництві яко використовується переробленний пластик?',
-      answers: [{ answer: 'поліестер' }, { answer: 'бабовна' }, { answer: 'інакше' }],
-    },
-    {
-      id: 2,
-      id_quiz: '1',
-      img_quest: '../img/ques_img_1.png',
-      question_title: 'використовується переробленний пластик?',
-      answers: [{ answer: 'поліестер' }, { answer: 'інакше' }],
-    },
-    {
-      id: 0,
-      id_quiz: '0',
-      img_quest: '../img/ques_img_1.png',
-      question_title:
-        'Як називається тканина, у виробництві яко використовується переробленний пластик?',
-      answers: [
-        { answer: 'шовк' },
-        { answer: 'бабовна' },
-        { answer: 'поліестер' },
-        { answer: 'інакше' },
-      ],
-    },
-    {
-      id: 1,
-      id_quiz: '0',
-      img_quest: '../img/ques_img_1.png',
-      question_title: 'виробництві яко використовується переробленний пластик?',
-      answers: [
-        { answer: 'шовк' },
-        { answer: 'бабовна' },
-        { answer: 'поліестер' },
-        { answer: 'інакше' },
-      ],
-    },
-    {
-      id: 2,
-      id_quiz: '1',
-      img_quest: '../img/ques_img_1.png',
-      question_title: 'використовується переробленний пластик?',
-      answers: [
-        { answer: 'шовк' },
-        { answer: 'бабовна' },
-        { answer: 'поліестер' },
-        { answer: 'інакше' },
-      ],
-    },
-  ],
-
-  countQuiz: [
-    {
-      id: 0,
-      count: 0,
     },
   ],
 };
