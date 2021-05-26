9b3d57801ccde2c2bcd28608baeeeee6d8016a5d
Ihor Chekhunov <Ihor Chekhunov.scdesignk@gmail.com>
2021-05-26 03:31:19 +0300

    added page quiz result layuot

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, render } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Header } from './components';
-import { Login, Home, Quiz } from './pages';
+import { Login, Home, Quiz, QuizResultPage } from './pages';
 import './scss/app.scss';
 
 function App(props) {
   return (
     <div className="wrapper">
       <Router>
+        <Switch>
+          <Route path="/" exact component={() => <Login />} />
+        </Switch>
+
         <Header />
         <div className="content">
-          <Switch>
-            <Route path="/" exact component={() => <Login />} />
-
-            <Route exact path="/home" render={() => <Home quiz={props.appState.quiz} />} />
-            <Route exact path="/quiz/:id" render={() => <Quiz quiz={props.appState.quiz} />} />
-          </Switch>
+          <Route exact path="/home" render={() => <Home quiz={props.appState.quiz} />} />
+          <Route exact path="/quiz/:id" render={() => <Quiz quiz={props.appState.quiz} />} />
+          <Route exact path="/quiz-result" component={() => <QuizResultPage />} />
         </div>
       </Router>
     </div>
