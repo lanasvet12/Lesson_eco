f5c499a24d76e8414a58394415fcb70ac3a71e18
Ihor Chekhunov <Ihor Chekhunov.scdesignk@gmail.com>
2021-05-26 23:54:00 +0300

    added new name component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Header } from './components';
-import { Login, Home, Quiz, QuizResultPage } from './pages';
+import { Login, Home, Quiz, StatisticsPage } from './pages';
 import './scss/app.scss';
 
 function App(props) {
@@ -17,7 +17,7 @@ function App(props) {
         <div className="content">
           <Route exact path="/home" render={() => <Home quiz={props.appState.quiz} />} />
           <Route exact path="/quiz/:id" render={() => <Quiz quiz={props.appState.quiz} />} />
-          <Route exact path="/quiz-result" component={() => <QuizResultPage />} />
+          <Route exact path="/statistics" component={() => <StatisticsPage />} />
         </div>
       </Router>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Login } from './Login';
 export { default as Home } from './Home';
 export { default as Quiz } from './Quiz';
-export { default as QuizResultPage } from './QuizResultPage';
+export { default as StatisticsPage } from './StatisticsPage';
